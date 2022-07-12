bb81d259346714ac5234aea7cab9df81640d94a9
Adam-Wilcock <adamwilcock10@gmail.com>
2022-07-12 09:47:01 +0100

    Re-factored View Models

--- InvoiceGenerator-dotnet-maui-UI/ViewModels/ClientDetailsViewModel.cs
+++ InvoiceGenerator-dotnet-maui-UI/ViewModels/ClientDetailsViewModel.cs
@@ -62,25 +62,4 @@ namespace InvoiceGenerator_dotnet_maui_UI.ViewModels
         }
 
     }
-
-    public class ClientViewModel
-    {
-        public Guid ClientId { get; set; }
-        public string ClientName { get; set; }
-        public string ClientAddress { get; set; }
-        public string ContactName { get; set; }
-        public string ContactEmail { get; set; }
-    }
-
-    public partial class BaseViewModel : ObservableObject
-    {
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
-        bool isBusy;
-
-        [ObservableProperty]
-        string title;
-
-        public bool IsNotBusy => !IsBusy;
-    }
 }
