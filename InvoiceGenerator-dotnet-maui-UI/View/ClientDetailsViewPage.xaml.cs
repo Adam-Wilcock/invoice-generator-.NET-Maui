5f8efc50d303a7f58a1187cc6e437bfa7f0ba853
Adam-Wilcock <adamwilcock10@gmail.com>
2022-07-12 10:21:33 +0100

    Removed commented code and Un-used class

--- InvoiceGenerator-dotnet-maui-UI/View/ClientDetailsViewPage.xaml.cs
+++ InvoiceGenerator-dotnet-maui-UI/View/ClientDetailsViewPage.xaml.cs
@@ -6,68 +6,9 @@ namespace InvoiceGenerator_dotnet_maui_UI;
 
 public partial class ClientDetailsViewPage : ContentPage
 {
-
-    //public ClientDetailsListViewModel ViewModel { get; set; }
-
     public ClientDetailsViewPage(ClientDetailsViewModel viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
-
-        //lst_clients.SetValue(ItemsView.ItemsSourceProperty, ViewModel);
-
-        //this.BindingContext = ViewModel;
-
-
-        //var allClients = _clientService.GetClients();
-        //if (allClients.Any())
-        //{
-        //    Monkeys.Clear();
-        //}
-        //foreach(var client in allClients)
-        //{
-        //    Monkeys.Add(new ClientDetailsViewModel
-        //    {
-        //        ClientAddress = client.ClientAddress,
-        //        ClientName = client.ClientName,
-        //        ContactEmail = client.ContactEmail,
-        //        ContactName = client.ContactName
-        //    });
-        //}
-
-        //var thing = allClients.Select(c => new ClientDetailsViewModel
-        //{
-        //    ClientName = c.ClientName,
-        //    ClientAddress = c.ClientAddress
-        //}).ToList();
-
-        //var viewModelList = new ClientDetailsListViewModel();
-        //viewModelList.ViewList = viewModels;
-    }
-}
-
-//public class ClientDetailsListViewModel : ViewModelBase
-//{
-//    private List<ClientDetailsViewModel> viewList;
-//    public List<ClientDetailsViewModel> ViewList
-//    {
-//        get => viewList;
-//        set => SetProperty(ref viewList, value);
-//    }
-//}
-
-public class ViewModelBase : INotifyPropertyChanged
-{
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    public bool SetProperty<T>(ref T field, T value, [CallerMemberName] string name = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value))
-        {
-            return false;
-        }
-        field = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        return true;
     }
 }
